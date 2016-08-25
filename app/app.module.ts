e9eaab0bd583008915afe744c9c5e786afcb7cb3
Jere <jereaa2@gmail.com>
2016-08-25 17:40:45 -0300

    Error fixed
    Noew everything works

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,7 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 
 import { AppComponent }  from './app.component';
+
 import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
 
 import { HeroService } from './hero.service'
 
@@ -14,7 +16,8 @@ import { HeroService } from './hero.service'
   ],
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   providers: [
     HeroService
