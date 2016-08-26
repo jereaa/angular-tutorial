bad0fb127d009823a9c16e4c599e86413c9b236a
Jere <jereaa2@gmail.com>
2016-08-26 01:45:06 -0300

    Finished routing and fixed dashboard bug

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,22 +1,26 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
 
-import { AppComponent }  from './app.component';
+import { AppComponent }   from './app.component';
+import { routing }        from './app.routing';
 
-import { HeroesComponent } from './heroes.component';
-import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent }      from './heroes.component';
+import { DashboardComponent }   from './dashboard.component';
+import { HeroDetailComponent }  from './hero-detail.component';
 
-import { HeroService } from './hero.service'
+import { HeroService }  from './hero.service';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    routing
   ],
   declarations: [
     AppComponent,
     HeroesComponent,
+    DashboardComponent,
     HeroDetailComponent
   ],
   providers: [
@@ -24,4 +28,6 @@ import { HeroService } from './hero.service'
   ],
   bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+
+export class AppModule {
+}
